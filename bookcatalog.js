401c1193dadc1b476eb7e90d9dc02fe3f468e684
yogeshjoshi <joshiyogesh001@gmail.com>
2016-06-23 20:04:54 +0530

    completed the updation function :: Still checking the unwanted case which can occur :: partially completed

--- bookcatalog.js
+++ bookcatalog.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var filename , input , input_tag , isbn_no , price , author , title , publisher , pages ;
+var filename , input , input_tag , isbn_no , price , author , title , publisher , pages , userinput;
 
 var inp_flg = input();
 if(inp_flg == 0){
@@ -11,19 +11,22 @@ if (valid == 0) {
 }
 mainbookcatalog();
 function input(){
-		var userinput = process.argv.slice(2,process.argv.length);
+		userinput = process.argv.slice(2,process.argv.length);
 		if(userinput[0]=="--help" || userinput[1]=="--help" || userinput[2]=="--help"){
 			var text = fs.readFileSync("helpBookCatalog.txt",'utf-8'); // filename is replaced by helpBookCatalog to show instant help to user
 			console.log(text);
 			return 0;
 		}
-if(userinput[2]==undefined || userinput[3]==undefined || userinput[4]==undefined || userinput[5]==undefined || userinput[6]==undefined||userinput[7]==undefined )
-	{
+		if(userinput[1] == "-add"){
+		if(userinput[2]==undefined || userinput[3]==undefined || userinput[4]==undefined || userinput[5]==undefined || userinput[6]==undefined||userinput[7]==undefined)
+		{
 		console.log("FIRST WRITE ALL INFORMATION REGARDING BOOK STORAGE");
 		console.log("INFORMATION SHOULD BE IN ORDER OF :: ISBN_NO PRICE AUTHOR TITLE PUBLISHER PAGES ");
 		console.log("For more info type --help ");
 		return 0;
 	}
+
+}
 		filename = userinput[0];
 		input_tag = userinput[1];
 		isbn_no = userinput[2];
@@ -58,10 +61,10 @@ function mainbookcatalog() {
 		return;
 	}
 
-	// else if(input_tag == "-update"){
-	// 	updateCatalog();
-	// 	return;
-	// }
+	else if(input_tag == "-update"){
+		updateCatalog();
+		return;
+	}
 
 	else if(input_tag == "-delete"){
 		deleteCatalog();
@@ -96,7 +99,7 @@ function deleteCatalog() {
 	}
 
 	if (count == 0) {
-		console.log("ISBN Number not found in File :: for more info type --help");
+		console.log("ISBN Number not found in File , To Add This ISBN NO in File use -add input tag:: for more info type --help");
 		return;
 	}
 	else{
@@ -115,4 +118,106 @@ function deleteCatalog() {
 function readCatalog(){
 	var text =fs.readFileSync(filename,'utf-8');
 	console.log(text);
+}
+
+function updateCatalog(){
+	if(isbn_no==undefined){
+		console.log("Enter the ISBN Number of BooK to be do updation in file ");
+		return;
+	}
+	var refrence_var,word_array,count=0;
+	var text = fs.readFileSync(filename,'utf-8');
+	var line_array = text.split("\n");
+	for (var i = 0; i < line_array.length-1; i++) {
+		word_array=line_array[i].split("\t");
+		if (word_array[0]==isbn_no){
+			count++;
+			refrence_var=i;
+		}
+	}
+	if (count == 0) {
+		console.log("ISBN Number not found in File , To add this use -add input tag :: for more info type --help");
+		return;
+	}
+	else{
+
+		word_array=line_array[refrence_var].split("\t");
+
+		if(userinput[3]=="-isbn"){
+			console.log("ISBN no can't be changed :: to delete isbn no press -delete isbn_no :: for more info press --help");
+			return;
+		}
+
+		else if(userinput[3] == "-price"){
+			if(userinput[4]==undefined){
+				console.log("Enter the Rquired Value To Change Or Modify");
+				return;
+			}
+			else{
+				line_array[refrence_var]=line_array[refrence_var].replace(word_array[1],userinput[4]);
+				fs.writeFileSync(filename,"",'utf-8');
+				for (var i = 0; i < line_array.length-1; i++) {
+			fs.appendFileSync(filename,line_array[i]+"\n",'utf-8');
+			}
+			}
+		}
+		else if(userinput[3]=="-author"){
+			if(userinput[4]==undefined){
+				console.log("Enter the Rquired Value To Change Or Modify");
+				return;
+			}
+			else{
+				line_array[refrence_var]=line_array[refrence_var].replace(word_array[2],userinput[4]);
+				fs.writeFileSync(filename,"",'utf-8');
+				for (var i = 0; i < line_array.length-1; i++) {
+			fs.appendFileSync(filename,line_array[i]+"\n",'utf-8');
+			}
+			}
+		}
+		else if(userinput[3]=="-title"){
+			if(userinput[4]==undefined){
+				console.log("Enter the Rquired Value To Change Or Modify");
+				return;
+			}
+			else{
+				line_array[refrence_var]=line_array[refrence_var].replace(word_array[3],userinput[4]);
+				fs.writeFileSync(filename,"",'utf-8');
+				for (var i = 0; i < line_array.length-1; i++) {
+			fs.appendFileSync(filename,line_array[i]+"\n",'utf-8');
+			}
+			}
+		}
+		else if(userinput[3]=="-publisher"){
+			if(userinput[4]==undefined){
+				console.log("Enter the Rquired Value To Change Or Modify");
+				return;
+			}
+			else{
+				line_array[refrence_var]=line_array[refrence_var].replace(word_array[4],userinput[4]);
+				fs.writeFileSync(filename,"",'utf-8');
+				for (var i = 0; i < line_array.length-1; i++) {
+			fs.appendFileSync(filename,line_array[i]+"\n",'utf-8');
+			}
+			}
+		}
+		else if(userinput[3]=="-pages"){
+			if(userinput[4]==undefined){
+				console.log("Enter the Rquired Value To Change Or Modify");
+				return;
+			}
+			else{
+				line_array[refrence_var]=line_array[refrence_var].replace(word_array[5],userinput[4]);
+				fs.writeFileSync(filename,"",'utf-8');
+				for (var i = 0; i < line_array.length-1; i++) {
+			fs.appendFileSync(filename,line_array[i]+"\n",'utf-8');
+			}
+			}
+		}
+		else {
+			console.log("Type Right Entry In Input :: For more info type --help");
+			return;
+		}
+	}
+
+
 }
